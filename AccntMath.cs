f255185967ef4d4ee2604ce898039af3e5439ca9
savjaylade84 <savjaylade84@gmail.com>
2022-09-06 18:05:51 +0800

    added my existing project

---
+++ AccntMath.cs
@@ -0,0 +1,5 @@
+public class AccntMathP{
+
+    public void setGrossIncome(Account account){}
+
+}
---
+++ SalaryCompute.cs
@@ -0,0 +1,17 @@
+using System;
+using System.IO;
+
+namespace SalaryCompute
+{
+    class MySalary
+    {
+        
+         static void Main(string[] args){
+
+            Console.WriteLine("Hello World");
+
+        }
+
+    }
+    
+}
